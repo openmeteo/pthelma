9a17bc7fa0936abdb270ce94a6572c452ffc588a
Antonis Christofides <anthony@itia.ntua.gr>
2009-11-17 15:55:37 +0200

    Added setup.py for easy_install of pthelma

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "pthelma",
+    version = "0.1",
+    license = "GPL3",
+    description = "Hydro/meteorological-related library, including timeseries",
+    author = "Antonis Christofides",
+    author_email = "anthony@itia.ntua.gr",
+    packages = find_packages(),
+)
