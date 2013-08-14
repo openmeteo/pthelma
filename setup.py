943d6587a878e446d0a6dc2bdf3062b0414202c3
Antonis Christofides <antonis@wikical.com>
2013-08-14 18:00:16 +0300

    Added scripts parameter (loggertodb) to setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pthelma",
-    version = "0.1",
+    version = "0.1.0",
     license = "GPL3",
     description = "Hydro/meteorological-related library, including timeseries",
     author = "Antonis Christofides",
     author_email = "anthony@itia.ntua.gr",
     packages = find_packages(),
+    scripts = ['bin/loggertodb'],
     test_suite = "tests"
 )
