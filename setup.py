8c6f08be6365fb939210e2c05aaf7d36a0c3ec89
Antonis Christofides <antonis@wikical.com>
2014-06-24 15:47:50 +0300

    Version 0.9a7

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ else:
 
 kwargs = {
     'name': "pthelma",
-    'version': "0.9a6",
+    'version': "0.9a7",
     'license': "GPL3",
     'description': "Hydro/meteorological timeseries library",
     'author': "Antonis Christofides",
