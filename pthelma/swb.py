2fa6e68028fb42e839721e70c83f18235046d272
Stavros Anastasiadis <anastasiadis.st00@gmail.com>
2015-03-09 18:04:30 +0200

    Update swb model and addition FC_IRT parameter (#23)

--- pthelma/swb.py
+++ pthelma/swb.py
@@ -37,65 +37,65 @@ class SoilWaterBalance(object):
         self.lowlim_theta_mm = self.fc_mm - self.raw_mm
         self.wbm_report = []
 
-    def Peff_calc(self, date):
+    def __Peff_calc__(self, date):
         temp_peff = self.precip[date] * self.peff
         temp_eteff = self.evap[date] * (1 - self.peff)
         if temp_peff <= temp_eteff:
             return 0
         return self.precip[date] * self.peff
 
-    def ks_calc(self, Dr):
+    def __ks_calc__(self, Dr):
         # eq (84)
         if (self.taw_mm - Dr) / (self.taw_mm - self.raw_mm) > 1:
             return 1
         return (self.taw_mm - Dr) / (self.taw_mm - self.raw_mm)
 
-    def ETc_calc(self, date, Dr):
-        return self.evap[date] * self.ks_calc(Dr) * self.kc
+    def __ETc_calc__(self, date, Dr):
+        return self.evap[date] * self.__ks_calc__(Dr) * self.kc
 
-    def irrigate_question(self, Dr_i):
+    def __irrigate_question__(self, Dr_i):
         if Dr_i >= self.raw_mm:
             return 1
         return 0
 
-    def Dr_i_1_calc(self, Dr_i, Inet_i):
+    def __Dr_i_1_calc__(self, Dr_i, Inet_i):
         if Dr_i - Inet_i > self.taw_mm:
             return self.taw_mm
         return Dr_i - Inet_i
 
-    def theta_calc(self, theta_init, Dr_i, Inet_i):
+    def __theta_calc__(self, theta_init, Dr_i, Inet_i, irr_event_days):
         if theta_init - Dr_i >= self.theta_s_mm:
             return self.theta_s_mm
         return self.fc_mm - Dr_i + Inet_i
 
-    def Inet_calc(self, Dr_i):
+    def __Inet_calc__(self, Dr_i, FC_IRT):
         if Dr_i >= self.raw_mm:
-            return Dr_i
+            return Dr_i * FC_IRT
         else:
             return 0.0
 
-    def irr_events_calc(self, irr_event_days, day, Dr_i):
+    def __irr_events_calc__(self, irr_event_days, day, Dr_i, FC_IRT):
         if len(irr_event_days) >= 1:
             if day not in irr_event_days:
                 return 0.0
             else:
-                return self.Inet_calc(Dr_i)
-        return self.Inet_calc(Dr_i)
+                return Dr_i * FC_IRT
+        return self.__Inet_calc__(Dr_i, FC_IRT)
 
-    def get_timedelta(self):
+    def __get_timedelta__(self):
         if self.precip.time_step.length_minutes == 1440:
             return datetime.timedelta(days=1)
         if self.precip.time_step.length_minutes == 60:
             return datetime.timedelta(hours=1)
         raise ValueError("Timeseries should be Daily or Hourly")
 
-    def RO_calc(self, Peff_i, theta_1):
+    def __RO_calc__(self, Peff_i, theta_1):
         if theta_1 - self.theta_s_mm + Peff_i <= 0:
             return 0.0
         else:
             return theta_1 - self.theta_s_mm + Peff_i
 
-    def check_forecast_Dr(self, Dr_historical, theta):
+    def __check_forecast_Dr__(self, Dr_historical, theta):
         # For aira usage: Aira needs two separate runs,
         # one with Daily-Historical values second with Hourly-Forecast values.
         # In order to connect the runs, only Dr_i  from historical values
@@ -105,29 +105,31 @@ class SoilWaterBalance(object):
         return self.fc_mm - theta
 
     def water_balance(self, theta_init, irr_event_days,
-                      start_date, end_date, Dr_historical=None):
+                      start_date, end_date, FC_IRT=1,
+                      Dr_historical=None):
         if len(self.wbm_report) >= 1:
             self.depletion_report = []
         # i = 0
-        theta = theta_init
-        # check_forecast_Dr for aira
-        Dr_i = self.check_forecast_Dr(Dr_historical, theta)
+        # __check_forecast_Dr__ for aira
+        Dr_i = self.__check_forecast_Dr__(Dr_historical, theta_init)
         CR = 0.0  # Static exists for later dev improvement
         DP = 0.0  # Static exists for later dev improvement
         # Next step preparation / (t-1) values
         Dr_i_1 = Dr_i
-        theta_i_1 = theta
-        delta = self.get_timedelta()
+        theta_i_1 = theta_init
+        delta = self.__get_timedelta__()
         step = start_date
         while step <= end_date:
-            Peff_i = self.Peff_calc(step)
-            RO_i = self.RO_calc(Peff_i, theta_i_1)
-            ETc = self.ETc_calc(step, Dr_i_1)
+            Peff_i = self.__Peff_calc__(step)
+            RO_i = self.__RO_calc__(Peff_i, theta_i_1)
+            ETc = self.__ETc_calc__(step, Dr_i_1)
             SWB = - Peff_i - CR + ETc + DP + RO_i
             Dr_i = Dr_i_1 + SWB
-            Inet_i = self.irr_events_calc(irr_event_days, step, Dr_i)
-            theta = self.theta_calc(theta_init, Dr_i, Inet_i)
-            Dr_i_1 = self.Dr_i_1_calc(Dr_i, Inet_i)
+            Inet_i = self.__irr_events_calc__(irr_event_days,
+                                              step, Dr_i, FC_IRT)
+            theta = self.__theta_calc__(theta_init, Dr_i,
+                                        Inet_i, irr_event_days)
+            Dr_i_1 = self.__Dr_i_1_calc__(Dr_i, Inet_i)
             theta_i_1 = theta
             theta_p = theta / (self.rd * self.rd_factor)
             self.wbm_report.append({'date': step,
@@ -139,11 +141,11 @@ class SoilWaterBalance(object):
                                     'Inet': Inet_i,
                                     'CR': CR,
                                     'ETo': self.evap[step],
-                                    'Ks': self.ks_calc(Dr_i_1),
+                                    'Ks': self.__ks_calc__(Dr_i_1),
                                     'ETc': ETc,
                                     'DP': DP,
                                     'theta': theta,
-                                    'irrigate': self.irrigate_question(Dr_i),
+                                    'irrigate': self.__irrigate_question__(Dr_i),
                                     'Ifinal': Inet_i / self.irr_effi,
                                     'Dr_1_next': Dr_i_1,
                                     'Theta_p': theta_p})
