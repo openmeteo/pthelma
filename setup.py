cb52ebd14bcb68ae542e55395a3ec5b4d3c15d0a
Antonis Christofides <anthony@itia.ntua.gr>
2015-05-14 14:10:49 +0300

    Version 0.11.1

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ else:
 
 kwargs = {
     'name': "pthelma",
-    'version': "0.11.0",
+    'version': "0.11.1",
     'license': "GPL3",
     'description': "Hydro/meteorological timeseries library",
     'author': "Antonis Christofides",
