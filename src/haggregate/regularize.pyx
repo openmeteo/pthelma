26d6a7bde4e616e26e64f87d9ef9e3bb65c0897d
Antonis Christofides <antonis@antonischristofides.com>
2025-06-08 13:29:09 +0300

    Don't use obsolete 'long' Python keyword

--- src/haggregate/regularize.pyx
+++ src/haggregate/regularize.pyx
@@ -63,7 +63,7 @@ def regularize(ts, new_date_flag="DATEINSERT", mode=RM.INTERVAL):
     # supported by numba and Cython
     max_flags_length = max(ts.data["flags"].str.len()) + 1 + len(new_date_flag)
     flags_dtype = "U" + str(max_flags_length)
-    ts_index = ts.data.index.values.astype(long)
+    ts_index = ts.data.index.values.astype(np.int64)
     ts_values = ts.data["value"].values
     ts_flags = ts.data["flags"].values.astype(flags_dtype)
     result_step = np.timedelta64(step).astype(int) * 1000
